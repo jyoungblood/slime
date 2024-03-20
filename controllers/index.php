7fb77ddf3cf1b9a569075420a73aa8a9f2cb6d66
jonathan youngblood <jonathan.youngblood@gmail.com>
2024-03-20 13:51:47 -0500

    use phpdotenv instead of settings.php

--- controllers/index.php
+++ controllers/index.php
@@ -1,5 +1,7 @@
 <?php
 
+// date_default_timezone_set('America/Chicago');
+// error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 
 use Slime\render;
 
@@ -10,7 +12,7 @@ $app->get('/', function ($req, $res, $args) {
   return render::hbs($req, $res, [
     'layout' => '_layouts/base',
     'template' => 'index',
-    'title' => $GLOBALS['site_title'],
+    'title' => $_ENV['site_title'],
   ]);
 
 });
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   ███████║ ███████╗ ██║ ██║ ╚═╝ ██║ ███████╗
   ╚══════╝ ╚══════╝ ╚═╝ ╚═╝     ╚═╝ ╚══════╝
                                         
-  1.2.1 - https://github.com/hxgf/slime
+  1.2.2 - https://github.com/hxgf/slime
 
 */
 
@@ -17,17 +17,20 @@
 use Slim\Factory\AppFactory;
 use VPHP\db;
 use Slime\render;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
-require 'settings.php';
+
+Dotenv::createImmutable(__DIR__)->load();
 
 $app = AppFactory::create();
 
 $app->addBodyParsingMiddleware();
 
-$GLOBALS['database'] = isset($GLOBALS['settings']['database']) ? db::init($GLOBALS['settings']['database']) : false;
+$GLOBALS['locals'] = [ 'year' => date('Y'), 'site_title' => isset($_ENV['SITE_TITLE']) ? $_ENV['SITE_TITLE'] : false, 'site_code' => isset($_ENV['SITE_CODE']) ? $_ENV['SITE_CODE'] : false, 'site_url' => isset($_ENV['SITE_URL']) ? $_ENV['SITE_URL'] : false ];
+$GLOBALS['database'] = isset($_ENV['DB_HOST']) ? \VPHP\db::init([ 'host' => $_ENV['DB_HOST'], 'name' => $_ENV['DB_NAME'], 'user' => $_ENV['DB_USER'], 'password' => $_ENV['DB_PASSWORD'] ]) : false;
 
-$errorMiddleware = $app->addErrorMiddleware(false, false, false);
+$errorMiddleware = isset($_ENV['SITE_MODE']) && $_ENV['SITE_MODE'] == 'development' ? $app->addErrorMiddleware(true, true, true) : $app->addErrorMiddleware(false, false, false);
 
 $errorMiddleware->setErrorHandler(\Slim\Exception\HttpNotFoundException::class, function (
   \Psr\Http\Message\ServerRequestInterface $request,
