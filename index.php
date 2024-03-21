ce3ec6feb017370873b54691f0405771fe8f6fb5
jonathan youngblood <jonathan.youngblood@gmail.com>
2024-03-20 19:28:16 -0500

    base construction and error handler updates

--- index.php
+++ index.php
@@ -13,18 +13,11 @@
 
 */
 
-
-use Slim\Factory\AppFactory;
-use VPHP\db;
-use Slime\render;
-use Dotenv\Dotenv;
-
 require __DIR__ . '/vendor/autoload.php';
 
-Dotenv::createImmutable(__DIR__)->load();
+\Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
-$app = AppFactory::create();
-
+$app = \Slim\Factory\AppFactory::create();
 $app->addBodyParsingMiddleware();
 
 $GLOBALS['locals'] = [ 'year' => date('Y'), 'site_title' => isset($_ENV['SITE_TITLE']) ? $_ENV['SITE_TITLE'] : false, 'site_code' => isset($_ENV['SITE_CODE']) ? $_ENV['SITE_CODE'] : false, 'site_url' => isset($_ENV['SITE_URL']) ? $_ENV['SITE_URL'] : false ];
@@ -32,16 +25,14 @@ $GLOBALS['database'] = isset($_ENV['DB_HOST']) ? \VPHP\db::init([ 'host' => $_EN
 
 $errorMiddleware = isset($_ENV['SITE_MODE']) && $_ENV['SITE_MODE'] == 'development' ? $app->addErrorMiddleware(true, true, true) : $app->addErrorMiddleware(false, false, false);
 
-$errorMiddleware->setErrorHandler(\Slim\Exception\HttpNotFoundException::class, function (
-  \Psr\Http\Message\ServerRequestInterface $request,
-  \Throwable $exception,
-  bool $displayErrorDetails,
-  bool $logErrors,
-  bool $logErrorDetails
-) {
-  return render::hbs($request, new \Slim\Psr7\Response(), [
-    'template' => '404',
-    'title' => '404 NOT FOUND',
+$errorMiddleware->setErrorHandler(\Slim\Exception\HttpNotFoundException::class, function ( \Psr\Http\Message\ServerRequestInterface $request, \Throwable $exception, bool $displayErrorDetails, bool $logErrors, bool $logErrorDetails ) {
+  return \Slime\render::hbs($request, new \Slim\Psr7\Response(), [
+    'template' => 'error',
+    'title' => '404 - NOT FOUND',
+    'data' => [
+      'status_code' => 404,
+      'error_message' => 'This page could not be found.'
+    ]
   ], 404);
 });
 
